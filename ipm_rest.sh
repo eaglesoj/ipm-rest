55ed323a476a69e6094c4c4490ae29a4c4517ea5
Justin Eagleson <jjeagleson@gmail.com>
2018-05-04 21:49:39 -0400

    initial

---
+++ ipm_rest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# GET example: ./ipm_rest.sh GET '1.0/topology/mgmt_artifacts/UxCrquDzYTeqK-zrMivHyA/references/to/contains' 
+# POST example: ./ipm_rest.sh POST 1.0/topology/mgmt_artifacts '{"arbitraryStringProperty":"A string is here","description":"Custom group description","displayLabel":"MyGroup2","keyIndexName":"group2"}'
+
+config="ipm_rest.config"
+rtype=${1}
+uri=${2}
+data=${3}
+
+# get config
+. ${config}
+
+# make request
+
+# no payload
+if [ "x$data" = "x" ]; then
+	CMD="curl --tlsv1.2 -s -v -k --request ${rtype} --url 'https://${apmhost}:8091/${uri}'  --header 'accept: application/json' --header 'Referer: https://${apmhost}:8091' --header \"authorization: Basic ${auth}\" --header 'content-type: application/json'"
+else
+# with payload
+	CMD="curl --tlsv1.2 -s -v -k --request ${rtype} --url 'https://${apmhost}:8091/${uri}'  --header 'accept: application/json' --header 'Referer: https://${apmhost}:8091' --header \"authorization: Basic ${auth}\" --header 'content-type: application/json' --data '${data}'"
+fi
+
+echo "running: ${CMD}"
+eval ${CMD}
